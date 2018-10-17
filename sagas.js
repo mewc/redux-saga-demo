9496c9deb195fc31867094c1dfc37c99e07d32bc
mewc <mikecarter771@gmail.com>
2018-10-18 09:52:15 +1100

    export incrAsync, show saga test cases
    - using babel test script

--- sagas.js
+++ sagas.js
@@ -5,7 +5,7 @@ import { put, takeEvery, all} from 'redux-saga/effects'
     console.log('Hello Sagas!')
 }
 
- function* incrementAsync(){
+export function* incrementAsync(){
     yield delay(1000)
     yield put({type: 'INCREMENT'})
 }
